4d2077c4423d0b2655862ba13607357c29f09a63
Dmitriy Strebkov <87489519+DEMOGORGANN@users.noreply.github.com>
2021-10-09 17:50:44 +0300

    start options REST API

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import { useState, useEffect } from "react";
+
 
 function App() {
-  return <div>авы</div>;
+
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+
+
+  useEffect(() => {
+    fetch("http://localhost:3000/items")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        // Примечание: важно обрабатывать ошибки именно здесь, а не в блоке catch(),
+        // чтобы не перехватывать исключения из ошибок в самих компонентах.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+  return (
+    <div>
+      {items.map(it => {
+        return (
+          <div>
+            <h1>{it.name}</h1>
+          </div>
+        )
+      })}
+
+    </div>
+  );
 }
 
 export default App;
